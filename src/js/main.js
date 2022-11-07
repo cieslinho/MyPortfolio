2fa455d46a76529e02b21e27865bca48cadd7ec9
Szymon Cieśla <cszymon01@gmail.com>
2022-11-07 15:29:41 +0100

    added validation to form in contact section, need to fix label

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,14 @@ const sliderWidth = 100
 const sliderSpeed = 3000
 let index = 0
 
+const nameInput = document.querySelector('#name')
+const mobileInput = document.querySelector('#mobile')
+const emailInput = document.querySelector('#email')
+const subjectInput = document.querySelector('#subject')
+const formLabel = document.querySelector('.contact__label')
+const textarea = document.querySelector('#textarea')
+const formBtn = document.querySelector('.contact__btn')
+
 const handleSlider = () => {
 	index++
 	changeImages()
@@ -51,6 +59,58 @@ const handleNav = () => {
 	}
 }
 
+const showError = input => {
+	const formField = input.parentElement
+	formField.classList.add('error')
+}
+
+const clearError = input => {
+	const formField = input.parentElement
+	formField.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el)
+		} else {
+			clearError(el)
+			// formLabel.classList.add('active-label')
+		}
+	})
+}
+
+const checkEmail = email => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(emailInput.value)) {
+		clearError(email)
+	} else {
+		showError(email, `E-mail is not correct`)
+	}
+}
+
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.contact__field')
+	let errorCount = 0
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+		console.log(el)
+	})
+
+	console.log(allInputs)
+	console.log(errorCount)
+}
+
 navBtn.addEventListener('click', handleNav)
 sliderRightBtn.addEventListener('click', handleRightArrow)
 sliderLeftBtn.addEventListener('click', handleLeftArrow)
+formBtn.addEventListener('click', e => {
+	e.preventDefault()
+	checkForm([nameInput, mobileInput, emailInput, subjectInput, textarea])
+	checkEmail(email)
+	checkErrors()
+})
